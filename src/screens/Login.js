1634e238d4faedaeaaaa8681bfe4dbe4c669cb1a
juumelisa <jumelisa09@gmail.com>
2023-03-19 17:53:49 +0700

    add google sign in for android

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -9,107 +9,112 @@ import {
 } from 'native-base';
 import {TouchableOpacity} from 'react-native';
 import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
+GoogleSignin.configure({
+  webClientId:
+    '622221729205-i7al3j8jmmkt79uaa2r5crnbj63a24ss.apps.googleusercontent.com',
+});
 const Login = ({navigation}) => {
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
-  useEffect(() => {
-    const onAuthStateChanged = users => {
-      setUser(users);
-      if (initializing) {
-        setInitializing(false);
-      }
-    };
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber;
-  }, [initializing]);
-
-  if (!user) {
-    return (
-      <NativeBaseProvider>
-        <View
-          width={'100%'}
-          height="100%"
-          justifyContent="center"
-          alignItems="center">
-          <Stack space={4} padding={10} width={'full'} maxWidth={400}>
-            <Text fontWeight="bold" textAlign="center" fontSize={24}>
-              Login
-            </Text>
-            <Input
-              variant="underlined"
-              borderBottomWidth={1}
-              borderBottomColor="#8E6737"
-              placeholder="Username or email"
-              fontSize={16}
-            />
-            <Input
-              variant="underlined"
-              borderBottomWidth={1}
-              borderBottomColor="#8E6737"
-              placeholder="Password"
-              fontSize={16}
-            />
-            <Button
-              fontSize={16}
-              marginY={3}
-              height={50}
-              backgroundColor="#8E6737"
-              onPress={() =>
+  // const [initializing, setInitializing] = useState(true);
+  // const [user, setUser] = useState();
+  // useEffect(() => {
+  //   const onAuthStateChanged = users => {
+  //     setUser(users);
+  //     if (initializing) {
+  //       setInitializing(false);
+  //     }
+  //   };
+  //   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+  //   return subscriber;
+  // }, [initializing]);
+  const onGoogleButtonPress = async () => {
+    try {
+      console.log('A');
+      await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+      console.log('B');
+      // Get the users ID token
+      const {idToken} = await GoogleSignin.signIn();
+      console.log('ID TOKEN: ', idToken);
+      console.log('C');
+      // Create a Google credential with the token
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      console.log('D');
+      // Sign-in the user with the credential
+      return auth().signInWithCredential(googleCredential);
+    } catch (err) {
+      console.log('ERROR NIH');
+      console.log(err);
+      return err;
+    }
+  };
+  return (
+    <NativeBaseProvider>
+      <View
+        width={'100%'}
+        height="100%"
+        justifyContent="center"
+        alignItems="center">
+        <Stack space={4} padding={10} width={'full'} maxWidth={400}>
+          <Text fontWeight="bold" textAlign="center" fontSize={24}>
+            Login
+          </Text>
+          <Input
+            variant="underlined"
+            borderBottomWidth={1}
+            borderBottomColor="#8E6737"
+            placeholder="Username or email"
+            fontSize={16}
+          />
+          <Input
+            variant="underlined"
+            borderBottomWidth={1}
+            borderBottomColor="#8E6737"
+            placeholder="Password"
+            fontSize={16}
+          />
+          <Button
+            fontSize={16}
+            marginY={3}
+            height={50}
+            backgroundColor="#8E6737"
+            onPress={() =>
+              navigation.navigate('Chat Message', {
+                screen: 'Chat',
+                params: {user: 'Jane'},
+              })
+            }>
+            Login
+          </Button>
+          <Button
+            onPress={() =>
+              onGoogleButtonPress().then(value =>
                 navigation.navigate('Chat Message', {
-                  screen: 'Chat',
-                  params: {user: 'Jane'},
-                })
-              }>
-              Login
-            </Button>
-            <View flexDirection="row">
-              <Text>Don't have an account?</Text>
-              <TouchableOpacity onPress={() => navigation.navigate('Register')}>
-                <Text underline> Register here</Text>
-              </TouchableOpacity>
-            </View>
-          </Stack>
-        </View>
-      </NativeBaseProvider>
-    );
-  } else {
-    console.log(user);
-    return (
-      <NativeBaseProvider>
-        <View
-          width={'100%'}
-          height="100%"
-          justifyContent="center"
-          alignItems="center">
-          <Stack space={4} padding={10} width={'full'} maxWidth={400}>
-            <Text fontWeight="bold" textAlign="center" fontSize={24}>
-              Welcome, {user.email}
-            </Text>
-            <Button
-              fontSize={16}
-              marginY={3}
-              height={50}
-              backgroundColor="#8E6737"
-              onPress={() =>
-                navigation.navigate('Chat Message', {
-                  screen: 'Chat',
-                  params: {user: 'Jane'},
-                })
-              }>
-              Login
-            </Button>
-            <View flexDirection="row">
-              <Text>Don't have an account?</Text>
-              <TouchableOpacity onPress={() => navigation.navigate('Register')}>
-                <Text underline> Register here</Text>
-              </TouchableOpacity>
-            </View>
-          </Stack>
-        </View>
-      </NativeBaseProvider>
-    );
-  }
+                  screen: 'Setting',
+                  params: {
+                    userInfo: {
+                      isNewUser: value.additionalUserInfo.isNewUser,
+                      uid: value.user.uid,
+                      photoURL: value.user.photoURL,
+                      displayName: value.user.displayName,
+                      email: value.user.email,
+                    },
+                  },
+                }),
+              )
+            }>
+            Login With Google
+          </Button>
+          <View flexDirection="row">
+            <Text>Don't have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+              <Text underline> Register here</Text>
+            </TouchableOpacity>
+          </View>
+        </Stack>
+      </View>
+    </NativeBaseProvider>
+  );
 };
 
 export default Login;
--- src/screens/Setting.js
+++ src/screens/Setting.js
@@ -1,11 +1,40 @@
+import {Image, Text, View} from 'native-base';
 import React from 'react';
-import {View, Text} from 'react-native';
+import Screen from '../components/Screen';
+import EnIcon from 'react-native-vector-icons/Entypo';
+// import {TouchableOpacity} from 'react-native';
 // import Icon from 'react-native-vector-icons/Ionicons';
-const Setting = () => {
+const Setting = ({navigation, route: params}) => {
+  console.log(params);
   return (
-    <View>
-      <Text>This is Home Screen</Text>
-    </View>
+    <Screen backgroundColor={'#DDD6CE'}>
+      <View
+        flexDirection={'row'}
+        alignItems={'center'}
+        padding={3}
+        backgroundColor={'#8E6737'}
+        borderRadius={'10px'}
+        marginTop={3}>
+        <Image
+          source={{
+            uri: params.params.userInfo.photoURL,
+          }}
+          alt={params.params.userInfo.displayName}
+          width={'70px'}
+          height={'70px'}
+          borderRadius={'35px'}
+        />
+        <View paddingLeft={3}>
+          <Text fontWeight={'bold'} fontSize={'18px'} color={'white'}>
+            {params.params.userInfo.displayName}
+          </Text>
+          <Text color={'white'}>{params.params.userInfo.email}</Text>
+        </View>
+        <View position={'absolute'} top={0} right={0} padding={3}>
+          <EnIcon name="edit" size={20} color={'white'} />
+        </View>
+      </View>
+    </Screen>
   );
 };
 
